ba100224405dfe67b2b03bc5d2d3973c1180ae17
victors <115093754+victors3136@users.noreply.github.com>
2025-05-05 18:24:31 +0300

    Add files via upload

---
+++ Tokenizer.py
@@ -0,0 +1,17 @@
+import spacy
+from spacy.tokens import Doc
+import Enum
+from Processor.CommonCore.supported_language import SupportedLanguage
+
+
+class Tokenizer(Enum):
+    _Italian = spacy.load("it_core_news_lg")
+    _Spanish = spacy.load("es_dep_news_trf")
+
+    @classmethod
+    def tokenize(cls, lang: SupportedLanguage, text: str) -> Doc:
+        match(lang):
+            case SupportedLanguage.Italian:
+                return cls._Italian(text)
+            case SupportedLanguage.Spanish:
+                return cls._Spanish(text)
