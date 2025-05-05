b4a3daaf784b4aa56269de61595eab8b97ed67b7
Victor Selegean <victor.selegean@garmin.com>
2025-05-05 23:51:06 +0300

    Backtrack
    Change-Id: If337306226b5073ecc97f3d7bb6788dc17856007

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
+git lfs install
+git lfs pull
 pip3 install -r requirements.txt
 python -m spacy download it_core_news_lg
 python -m spacy download es_dep_news_trf
--- setup_dev.sh
+++
@@ -1,4 +0,0 @@
-git lfs install
-git lfs pull
-chmod 777 ./setup_run.sh
-./setup_run.sh
