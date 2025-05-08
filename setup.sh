7f191497e349aae9d2bbb710690355a88118db27
victors <115093754+victors3136@users.noreply.github.com>
2025-05-08 13:11:21 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 git lfs install
 git lfs pull
 source ./.venv/bin/activate
-pip3 install -r requirements.txt
+pip3 install -r --force-reinstall requirements.txt
 python -m spacy download it_core_news_lg
 python -m spacy download es_dep_news_trf
