e8c5199a4ed04753462fc3716245dff0437ac86c
Victor Selegean <victor.selegean@garmin.com>
2025-05-17 17:28:52 +0300

    Formatting
    Change-Id: I5c856ed0e10c4513a3275830342433179a0cbc1d

--- Benchmark/benchmark.py
+++ Benchmark/benchmark.py
@@ -1,9 +1,8 @@
 import os
 from datasets import load_dataset
 from jiwer import wer, cer
-from transformers.models.whisper import WhisperProcessor, WhisperForConditionalGeneration
-from transformers import AutoProcessor, AutoModelForSpeechSeq2Seq
-from transformers.generation.configuration_utils import GenerationConfig
+from transformers.models.auto.processing_auto import AutoProcessor
+from transformers.models.auto.modeling_auto import AutoModelForSpeechSeq2Seq
 import torchaudio
 import torch
 from tqdm import tqdm
@@ -15,9 +14,9 @@ MAX_SAMPLES = 1_000
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 
 print("Loading Common Voice Romanian test data...")
-cv = load_dataset("mozilla-foundation/common_voice_11_0", LANGUAGE, split=SPLIT)
-cv = cv.filter(lambda x: x["sentence"] is not None and x["audio"] is not None)
-cv = cv.select(range(min(MAX_SAMPLES, len(cv))))
+dataset = load_dataset("mozilla-foundation/common_voice_11_0", LANGUAGE, split=SPLIT)
+dataset = dataset.filter(lambda x: x["sentence"] is not None and x["audio"] is not None)
+dataset = dataset.select(range(min(MAX_SAMPLES, len(dataset))))
 
 def speech_file_to_array_fn(batch):
     speech_array, _ = torchaudio.load(batch["audio"]["path"])
@@ -25,7 +24,7 @@ def speech_file_to_array_fn(batch):
     batch["target_text"] = batch["sentence"].lower()
     return batch
 
-cv = cv.map(speech_file_to_array_fn)
+dataset = dataset.map(speech_file_to_array_fn)
 
 results = []
 for model_id in MODEL_PATHS:
@@ -41,10 +40,10 @@ for model_id in MODEL_PATHS:
     predictions = []
     references = []
 
-    for sample in tqdm(cv):
+    for sample in tqdm(dataset):
         input_features = processor(
             sample["speech"],
-            sampling_rate=16000,
+            sampling_rate=16_000,
             return_tensors="pt"
         ).input_features.to(DEVICE)
         with torch.no_grad():
@@ -59,12 +58,12 @@ for model_id in MODEL_PATHS:
                 continue
         transcription = processor.batch_decode(predicted_ids, skip_special_tokens=True)[0].lower()
 
-        predictions.append(transcription)
-        references.append(sample["target_text"])
+        predictions.append(transcription.lower())
+        references.append(sample["target_text"].lower())
 
     wer_score = wer(references, predictions) * 100
     cer_score = cer(references, predictions) * 100
-    print(f"Total samples: {len(cv)}, of which {fails} failed due to indexing errors")
+    print(f"Total samples: {len(dataset)}, of which {fails} failed due to indexing errors")
     print(f"{model_id}: WER = {wer_score:.2f}%, CER = {cer_score:.2f}%")
     results.append((model_id, wer_score, cer_score))
 
